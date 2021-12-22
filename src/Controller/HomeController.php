d6aab3a57a2cd51f0cda3e8f51b190e9cd746674
Nathalie Lavroff <nathalielavroff@gmail.com>
2021-12-22 18:02:24 +0100

    add pagination

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,14 +3,31 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('', name: 'home')]
+
+    public function listAction(ArticleRepository $articleRepository, PaginatorInterface $paginator, Request $request)
+    {
+        $query = $articleRepository->createQueryBuilder('a')->getQuery();
+
+        $pagination = $paginator->paginate(
+            $query,
+            $request->query->getInt('page', 1),
+            3
+        );
+
+        return $this->render('home/index.html.twig', ['pagination' => $pagination]);
+    }
+
     public function index(ManagerRegistry $doctrine): Response
     {
          $articles = $doctrine->getRepository(Article::class)
